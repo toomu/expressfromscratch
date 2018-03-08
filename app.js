3ca3aaf8da27b69165f5ab70d97f3d40aa5757bb
ananth varma <ananthavarma.tech@gmail.com>
2018-03-08 15:24:50 +0530

    signup validation done

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require("express");
 var http = require('http');
 var path = require('path');
 var expressValidator = require("express-validator");
-var flash = require('connect-flash');
+// var flash = require('connect-flash');
 
 var mongoose = require("mongoose");
 
 var app = express();
+var bodyParser = require('body-parser');
 
 
 var session = require("express-session");
@@ -40,8 +41,11 @@ app.use(cookieParser())
 app.use(function(req, res, next) {
   console.log('Cookies: ', req.cookies);
   next();
-})
+});
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var logger = require('morgan');
 app.use(logger('dev'));
@@ -53,6 +57,7 @@ mongoose.connection.on("error", function(err){
   process.exit();
 
 });
+
 
 var port = 3000
 app.set('port', port);
@@ -72,15 +77,14 @@ app.use(function(req, res, next) {
     next();
 });
 
+
+
 var router = express.Router();
 
 var router2 = express.Router();
 
 app.use(router)
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(router2)
 
@@ -191,7 +195,7 @@ router2.get('/download/:file', function(req,res,next){
   res.sendFile(__dirname + "/uploads/" + req.params.file);
 });
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var userSchema = mongoose.Schema({
     firstName: { type:String,required:true},
@@ -206,106 +210,123 @@ var User = mongoose.model('User', userSchema);
 
 router2.post('/signup', function(req, res, next) {
 
-    req.checkBody('firstname','firstname is required').notEmpty;
-    req.checkBody('lastname','lastname is required').notEmpty;
-    req.checkBody('email','Invalid email address').notEmpty.isEmail();
-    req.checkBody('password','password is invalid').isLength({min:6}).equals(req.body.confirmpassword);
+    req.checkBody('firstName','firstname is required').notEmpty();
+    req.checkBody('lastName','lastname is required').notEmpty();
+    req.checkBody('email', 'email is required').notEmpty();
+    req.checkBody('email', 'Invalid email address').isEmail();
+    req.checkBody('password', 'password required').notEmpty();
+    req.checkBody('password', 'password is short - min 6 char required').isLength({min: 6});
+
+    var errors = req.validationErrors();
 
 
-    var user = new User({ firstName:req.body.firstname ,lastName:req.body.lastname,yourEmail:req.body.email,yourPassword:req.body.password});
-    user.save(function(err){
-        if(err){
-            console.log(err);
-            res.json({"status":err})
-        }else{
-            res.json({"status":"success"});
-        }
-    });
+    console.log(errors);
+    if (errors) {
+        res.json({"status": errors})
+    } else {
 
+
+        var user = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            yourEmail: req.body.email,
+            yourPassword: req.body.password,
+        });
+
+        user.save(function (err) {
+            if (err) {
+                console.log(err);
+                res.json({"status": err})
+            } else {
+                res.json({"status": "success"})
+            }
+        });
+    }
 });
 
 
-router2.get('/users', function(req, res, next) {
 
-    User.find({}, function(err, user) {
+    router2.get('/users', function (req, res, next) {
 
-        if(err){
-            res.json({err:err});
-        }else{
-            res.send(user);
-        }
+        User.find({}, function (err, user) {
+
+            if (err) {
+                res.json({err: err})
+            } else {
+                res.send(user);
+            }
+        });
     });
-});
 
-router2.get('/users/:id', function(req, res, next) {
+    router2.get('/users/:id', function (req, res, next) {
 
-    User.findById(req.params.id, function(err, user) {
-        if(err){
-            res.json({err:err});
-        }else{
-            res.json(user);
-        }
+        User.findById(req.params.id, function (err, user) {
+            if (err) {
+                res.json({err: err});
+            } else {
+                res.json(user);
+            }
+        });
     });
-});
 
-router2.delete('/users/:id', function(req, res, next) {
+    router2.delete('/users/:id', function (req, res, next) {
 
-    console.log(req.params);
+        console.log(req.params);
 
-    User.remove({ _id: req.params.id}, function(err) {
+        User.remove({_id: req.params.id}, function (err) {
+            if (err) {
+                res.json({err: err});
+            }
+            else {
+                res.json({
+                    status: "success"
+                })
+            }
+        });
+    });
+
+    router2.patch('/users/:id', function (req, res, next) {
+
+
+        console.log(req.params);
+
+        User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                firstName: req.body.firstname,
+                lastName: req.body.lastname,
+                yourEmail: req.body.email,
+                yourPassword: req.body.password
+            }
+        }, {new: true}, function (err, user) {
+            if (err) {
+                res.json({err: err})
+            } else {
+                res.json({user: user})
+            }
+        });
+
+    });
+//
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    app.use(require('connect-flash')());
+    app.use(function (req, res, next) {
+        res.locals.messages = require('express-messages')(req, res);
+        next();
+    });
+
+    app.use(function (err, req, res, next) { //errrenderfucntion
+
         if (err) {
-            res.json({err:err});
+
+            res.locals.message = err.message;
+            console.log(err);
+
+            res.json({err: err});
         }
-        else {
-            res.json({
-                status:"success"
-            })
-        }
-    });
-});
 
-router2.patch('/users/:id', function(req, res, next) {
-
-
-    console.log(req.params);
-
-    User.findByIdAndUpdate(req.params.id, { $set: {firstName:req.body.firstname ,lastName:req.body.lastname,yourEmail:req.body.email,yourPassword:req.body.password}},{new:true}, function (err, user) {
-        if (err){
-            res.json({err:err})
-        }else{
-            res.json({user:user});
-        }
-    });
-
-});
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-    res.locals.messages = require('express-messages')(req, res);
-    next();
-});
-
-app.use(function(err, req,res, next){ //errrenderfucntion
-
-  if(err){
-
-    res.locals.message = err.message;
-    console.log(err);
-
-    res.json({err:err, common:"common"});
-  }
-
-})
-
+    })
 // console.log(process)
 // process.exit();
-
-//morgan, bodyparser, creator router, router2 , errfunction, errrenderfunction
-
-
-
-
-
-
+//
+// morgan, bodyparser, creator router, router2 , er
